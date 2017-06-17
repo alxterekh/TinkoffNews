2d05307703c6f4d576fbbc024c151e030fd490b6
Alexander Terekhov <alexander.terekh@gmail.com>
2017-06-17 12:40:03 +0300

    added initial project

---
+++ TinkoffNews/TinkoffNews/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TinkoffNews
+//
+//  Created by Alexander on 17/06/2017.
+//  Copyright © 2017 Alexander Terekhov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ TinkoffNews/TinkoffNews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Alexander on 17/06/2017.
+//  Copyright © 2017 Alexander Terekhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
